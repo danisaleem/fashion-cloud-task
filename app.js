8ed58ec8ff5bc6857f8ceef9db1687fbe505dfa7
danialeem <daniyalsaleem.d@gmail.com>
2022-10-02 18:37:02 +0200

    send response if route does not exist

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ const port = process.env.PORT || 8091;
 // parse requests of content-type - application/json
 app.use(express.json());
 
+// log all endpoints
+app.all("*", (req, res, next) => {
+  console.log("req.url", req.url);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json("Welcome to Fashion Cloud Technical Task!");
 });
 
 app.use("/cache", cacheRoutes);
+
+// If No route matches then handle invalid requests here
+app.all("*", (req, res) => {
+  res.status(404).send({
+    message: "The resource you are looking for does not exist.",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
