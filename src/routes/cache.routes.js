0acb797c88fefd19277e70e32937d12261f875b8
danialeem <daniyalsaleem.d@gmail.com>
2022-10-02 18:35:52 +0200

    added required routes

--- src/routes/cache.routes.js
+++ src/routes/cache.routes.js
@@ -2,7 +2,19 @@ const cacheController = require("../controllers/cache.controller");
 const express = require("express");
 const router = express.Router();
 
-// Create a new Cache entry
+// returns the cached data for a given key
+router.get("/:id", cacheController.findOne);
+
+// returns all stored keys in the cache
+router.get("/", cacheController.findAll);
+
+// create and update the data for a given key
 router.post("/", cacheController.create);
+
+// removes a given key from the cache
+router.delete("/:id", cacheController.delete);
+
+// removes all keys from the cache
+router.delete("/", cacheController.deleteAll);
 
 module.exports = router;
